b8d97c120844847cd2169e0e758431c76bd999e3
kathrinmzl <kathrin@muenzel.de>
2025-09-19 11:17:30 +0200

    chore: Connect CI PostgreSQL Database

--- runnershive/settings.py
+++ runnershive/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+# for postgreSQL database
+import dj_database_url
 # env.py needs to be imported to be accessible
 if os.path.isfile('env.py'):
-    import env  # noqa
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +79,16 @@ WSGI_APPLICATION = 'runnershive.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Connection to CI PostgreSQL Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
