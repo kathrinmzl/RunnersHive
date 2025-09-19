a885fee0b3fa9ea1e2c0e4314f6286b2e5afe14e
kathrinmzl <kathrin@muenzel.de>
2025-09-19 10:38:10 +0200

    chore: Set up SECRET_KEY environment variable for project

--- runnershive/settings.py
+++ runnershive/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+# env.py needs to be imported to be accessible
+if os.path.isfile('env.py'):
+    import env  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!1dvmf#-^ub8eg*c*gdr)^6&ioklelp^l#@xg(dxc!2uikg*b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
