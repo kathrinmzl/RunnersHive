75756dd80999b607dc94bcca7d29af8d62e57366
kathrinmzl <kathrin@muenzel.de>
2025-09-19 10:13:11 +0200

    chore: Prepare basic deployment to Heroku

--- runnershive/settings.py
+++ runnershive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--!1dvmf#-^ub8eg*c*gdr)^6&ioklelp^l#@xg(dxc!2uikg*b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
 ]
 
 MIDDLEWARE = [
--- runnershive/urls.py
+++ runnershive/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from events import views as index_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('events/', index_views.index, name='event'),
 ]
