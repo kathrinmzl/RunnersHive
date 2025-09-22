85d134112b4d2de1b0e22b6b868ce984a876db7d
kathrinmzl <kathrin@muenzel.de>
2025-09-22 13:19:17 +0200

    feat: Add date filter to event list

--- events/forms.py
+++ events/forms.py
@@ -3,13 +3,27 @@ from .models import Category, Event
 
 
 class EventFilterForm(forms.Form):
+
     category = forms.ModelMultipleChoiceField(
         queryset=Category.objects.all(),
         required=False,
         widget=forms.CheckboxSelectMultiple
     )
+
     difficulty = forms.MultipleChoiceField(
         choices=Event.Difficulty.choices,
         required=False,
         widget=forms.CheckboxSelectMultiple
+    )
+
+    date_filter = forms.ChoiceField(
+        choices=[
+            ('today', 'Today'),
+            ('tomorrow', 'Tomorrow'),
+            ('this_week', 'This Week'),
+            ('all', 'All')
+        ],
+        required=False,
+        widget=forms.RadioSelect(attrs={'class': 'form-check-input'}),
+        initial='all'  # Preselect "All"
     )
--- events/views.py
+++ events/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from datetime import date
+from datetime import date, timedelta
 from urllib.parse import urlencode
 from .models import Event, Category
 from .forms import EventFilterForm
 
+# https://bastakiss.com/blog/django-6/enhancing-django-listview-with-dynamic-filtering-a-step-by-step-guide-403
 
 class EventListView(ListView):
     model = Event
@@ -26,12 +27,29 @@ class EventListView(ListView):
         self.form = EventFilterForm(self.request.GET or None)
 
         if self.form.is_valid():
+            # Category
             categories = self.form.cleaned_data.get("category")
-            difficulties = self.form.cleaned_data.get("difficulty")
             if categories:
                 queryset = queryset.filter(category__in=categories).distinct()
+            
+            # Difficulty
+            difficulties = self.form.cleaned_data.get("difficulty")
             if difficulties:
                 queryset = queryset.filter(difficulty__in=difficulties)
+
+            # Date
+            date_filter = self.form.cleaned_data.get('date_filter')
+            today = date.today()
+            if date_filter == 'today':
+                queryset = queryset.filter(date=today)
+            elif date_filter == 'tomorrow':
+                queryset = queryset.filter(date=today + timedelta(days=1))
+            elif date_filter == 'this_week':
+                # Assuming week starts on Monday
+                start_of_week = today - timedelta(days=today.weekday())
+                end_of_week = start_of_week + timedelta(days=6)
+                queryset = queryset.filter(date__range=[start_of_week, end_of_week])
+            # 'all' -> no date filter
 
         return queryset
 
