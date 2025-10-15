a59734600b51f689c6b81fea1d9a0cb6868e401b
kathrinmzl <kathrin@muenzel.de>
2025-10-15 17:37:39 +0200

    chore: Remove whitespace

--- runnershive/urls.py
+++ runnershive/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('events/', include("events.urls")),
     path('summernote/', include('django_summernote.urls')),
     path('', views.TodaysEventsListView.as_view(), name="home"),
-    path('', include("core.urls")), 
+    path('', include("core.urls")),
 ]
 
 handler400 = "runnershive.views.handler400"
